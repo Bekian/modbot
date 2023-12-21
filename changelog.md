e39c8f0cfdfeb0bea8d67d870308d7e768ede414
Bekian <Bekian.Cat@gmail.com>
2023-12-20 20:10:07 -0600

    index and changelog

---
+++ changelog.md
@@ -0,0 +1,5 @@
+# December 20th V0.0.1
+    - init
+    - Added changelog
+    - Added index.js from tutorial
+    - Fixed some eslint rules because the tutorial ones were bad
---
+++ index.js
@@ -0,0 +1,14 @@
+// get discordjs classes
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+// create client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// when the client is ready, display ready/login message
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// login using token from config file
+client.login(token);
